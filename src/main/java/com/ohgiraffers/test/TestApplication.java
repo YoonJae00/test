226158e910a7daf0c10439bbaedf4f87d4999caa
realyoon77 <realyoon77@gmail.com>
2024-01-31 13:09:35 +0900

    [test] 프로그램 test

---
+++ src/main/java/com/ohgiraffers/test/TestApplication.java
@@ -0,0 +1,22 @@
+package com.ohgiraffers.test;
+
+import org.w3c.dom.ls.LSOutput;
+
+public class TestApplication {
+    // 수업목표 오늘의 수업 목표입니다.
+    //여기는 주석입니다.
+    /* 여기는
+    주석 입니다. */
+    /* ctrl - space : 자동완성 기능
+       ctrl - d : 행복사 */
+
+    //복사예시
+    //복사예시
+    //복사예시
+
+    public static void main(String[] args) {
+        System.out.println("저는 개발자 입니다.");
+    }
+}
+
+
